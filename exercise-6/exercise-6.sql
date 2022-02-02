87eaa88f336bae72481f6fd3755f4cb05f7a9673
Katie Schrattenholzer <ktschr11@gmail.com>
2022-02-02 13:26:04 -0800

    exercise 7

--- exercise-6/exercise-6.sql
+++ exercise-6/exercise-6.sql
@@ -1,3 +1,18 @@
 -- customer ranking by total amount spent
 -- include email and rank
 -- use RANK ORDER BY the SUM of the amount in descending order
+SELECT 
+  customer.email,
+  RANK() OVER (ORDER BY SUM(payment.amount) DESC)
+FROM
+  customer
+INNER JOIN
+  payment 
+ON
+  customer.customer_id = payment.customer_id
+GROUP BY
+  customer.customer_id
+ORDER BY
+  RANK ASC
+;
+
