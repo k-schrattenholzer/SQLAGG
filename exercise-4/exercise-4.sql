b43a142d37aac812d14654301063ae625b2b450b
Katie Schrattenholzer <ktschr11@gmail.com>
2022-02-02 12:20:01 -0800

    exercise 4

--- exercise-4/exercise-4.sql
+++ exercise-4/exercise-4.sql
@@ -1,2 +1,10 @@
 -- the average film length by category
 -- include the category name and avg length
+SELECT category.name, AVG(film.length)
+FROM category
+INNER JOIN film_category
+ON film_category.category_id = category.category_id
+INNER JOIN film
+ON film_category.film_id = film.film_id
+GROUP BY category.name
+;
