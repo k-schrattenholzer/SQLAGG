1ff3051a4c4accf897ba763611ef244a317da825
Katie Schrattenholzer <ktschr11@gmail.com>
2022-02-02 11:49:37 -0800

    exercise 1

--- exercise-1/exercise-1.sql
+++ exercise-1/exercise-1.sql
@@ -3,3 +3,12 @@
 -- GROUP BY the film_id
 -- ORDER in descending order
 -- LIMIT 10
+SELECT film.title, COUNT(rental.rental_date)
+FROM inventory
+LEFT JOIN film
+ON inventory.film_id = film.film_id
+LEFT JOIN rental
+on inventory.inventory_id = rental.inventory_id
+GROUP BY film.film_id
+ORDER BY count DESC
+LIMIT 10;
